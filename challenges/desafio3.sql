0de743297a3e5adcf013d135493c9b96e6c512b4
jimmyarbats <jimmyaraujob@gmail.com>
2023-03-07 16:32:19 -0300

    finishing req 3

---
+++ challenges/desafio3.sql
@@ -0,0 +1,9 @@
+SELECT 
+    usr.name_user AS "pessoa_usuaria", 
+    COUNT(hst.id_music ) AS "musicas_ouvidas", 
+    ROUND(SUM(mus.duration_music)/60, 2) AS "total_minutos"
+  FROM users usr
+  JOIN history hst ON usr.id_user = hst.id_user
+  JOIN musics mus ON hst.id_music = mus.id_music
+  GROUP BY usr.name_user
+  ORDER BY usr.name_user;
