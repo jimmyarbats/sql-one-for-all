0f82e3da6d7f9e8513238ad1ec8e4339481ec74b
jimmyarbats <jimmyaraujob@gmail.com>
2023-03-07 16:33:58 -0300

    finishing req 4

---
+++ challenges/desafio4.sql
@@ -0,0 +1,7 @@
+SELECT usr.name_user as pessoa_usuaria,
+    IF(YEAR(MAX(hst.reproduction_date_history)) >= '2021', 'Ativa', 'Inativa')
+    AS status_pessoa_usuaria
+  FROM users usr
+  JOIN history hst ON hst.id_user = usr.id_user
+  GROUP BY pessoa_usuaria
+  ORDER BY pessoa_usuaria;
